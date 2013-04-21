d74878f9bf993b02a8e0f28d01822cd58fa33dd8
Jorge Cisneros <fraxito@gmail.com>
2013-04-22 00:20:56 +0200

    Prototipo inicial
    Tienes que hacer un FORK de este

---
+++ Buscaminas/Buscaminas/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Buscaminas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(284, 262);
+            this.panel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Buscaminas/Buscaminas/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Buscaminas
+{
+    public partial class Form1 : Form
+    {
+        //declaro el array de botones
+        Button[,] matrizBotones;
+
+        public Form1()
+        {
+            InitializeComponent();
+            int filas = 15;
+            int columnas = 20;
+            int anchoBoton = 20;
+
+            this.Height = columnas * anchoBoton + 40;
+            this.Width = filas * anchoBoton + 20;
+
+            matrizBotones = new Button[filas, columnas];
+
+            for (int i = 0; i < filas; i++)
+                for (int j = 0; j < columnas; j++)
+                {
+                    Button boton = new Button();
+                    boton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+                    boton.Width = anchoBoton;
+                    boton.Height = anchoBoton;
+                    boton.Location = new Point(i * anchoBoton, j * anchoBoton);
+                    boton.Click += chequeaBoton;
+                    matrizBotones[i, j] = boton;
+                    panel1.Controls.Add(boton);
+                }
+        }
+
+        private void chequeaBoton(object sender, EventArgs e)
+        {
+            (sender as Button).Enabled = false;
+
+        }
+    }
+}

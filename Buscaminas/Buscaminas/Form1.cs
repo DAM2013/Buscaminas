c7aaea7d205ec53b08086e2415781ad419f62f08
Jorge Cisneros <fraxito@gmail.com>
2013-04-22 23:11:26 +0200

    Clic y chequea alrededor

--- Buscaminas/Buscaminas/Form1.cs
+++ Buscaminas/Buscaminas/Form1.cs
@@ -21,13 +21,13 @@ namespace Buscaminas
     {
         //declaro el array de botones
         Button[,] matrizBotones;
-
+        int filas = 15;
+        int columnas = 20;
+        int anchoBoton = 20;
         public Form1()
         {
             InitializeComponent();
-            int filas = 15;
-            int columnas = 20;
-            int anchoBoton = 20;
+            
 
             this.Height = columnas * anchoBoton + 40;
             this.Width = filas * anchoBoton + 20;
@@ -38,7 +38,7 @@ namespace Buscaminas
                 for (int j = 0; j < columnas; j++)
                 {
                     Button boton = new Button();
-                    boton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+                    //boton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
                     boton.Width = anchoBoton;
                     boton.Height = anchoBoton;
                     boton.Location = new Point(i * anchoBoton, j * anchoBoton);
@@ -51,7 +51,25 @@ namespace Buscaminas
 
         private void chequeaBoton(object sender, EventArgs e)
         {
-            (sender as Button).Enabled = false;
+            Button b = (sender as Button);
+            int columna  = b.Location.X /anchoBoton;
+            int fila = b.Location.Y / anchoBoton;
+
+            //colorea en la fila del clic, el boton izquierdo y el derecho
+            matrizBotones[columna - 1, fila].BackColor = Color.Blue;
+            matrizBotones[columna, fila].BackColor = Color.Blue;
+            matrizBotones[columna + 1, fila].BackColor = Color.Blue;
+
+            //colorea en la fila superior del clic, el boton izquierdo, el central y el derecho
+            matrizBotones[columna - 1, fila - 1].BackColor = Color.Blue;
+            matrizBotones[columna, fila-1].BackColor = Color.Blue;
+            matrizBotones[columna + 1, fila-1].BackColor = Color.Blue;
+
+            //colorea en la fila inferior del clic, el boton izquierdo, el central y el derecho
+            matrizBotones[columna - 1, fila + 1].BackColor = Color.Blue;
+            matrizBotones[columna, fila + 1].BackColor = Color.Blue;
+            matrizBotones[columna + 1, fila + 1].BackColor = Color.Blue;
+
 
         }
     }
